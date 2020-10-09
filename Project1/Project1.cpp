b4ed9467eab94c6f84effc65f2866a61ea2e743d
Robrecht54 <robrecht.vanhoutte@gmail.com>
2020-10-09 21:46:09 +0200

    file

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -1,20 +1,33 @@
-// Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
+#include <stdlib.h>
+#include <stdio.h>
 int main()
 {
-    std::cout << "Hello World!\n";
+	float getal_a, getal_b, teller, som;
+
+	printf("\nTeller\t\tGetal\t\tMin\t\tMax\t\tsom\t\tGem");
+	printf("\n_ _ _\t\t_ _ _\t\t_ _\t\t_ _\t\t_ _\t\t_ _");
+	teller = 0;
+	getal_b = 0;
+	do
+	{
+		printf("\n%.f", teller = teller + 1);
+		printf("\t\t");
+		scanf_s("%f", &getal_a);
+
+		if (getal_a < getal_b)
+		{
+			printf("\t%.f", getal_a); /*minimum*/
+			printf("\t%.f", getal_b); /*maximum*/
+		}
+		if (getal_a > getal_b)
+		{
+			printf("\t%.f", getal_b); /*minimum*/
+			printf("\t%.f", getal_a); /*maximum*/
+		}
+		printf("\t%.f", getal_a + getal_b);
+		printf("\t%.2f", ((getal_a + getal_b) / 2));
+		(getal_b = getal_a + 0);
+
+	} while (teller < 10);
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
